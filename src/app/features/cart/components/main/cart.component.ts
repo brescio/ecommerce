813b3c2a22ca17dbcc2898ab2047ed722fa3ebec
brescio <57990413+brescio@users.noreply.github.com>
2020-09-23 13:56:37 +0200

    fix carrello

--- src/app/features/cart/components/main/cart.component.ts
+++ src/app/features/cart/components/main/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { CartItem } from 'src/app/core/model/cart-item.interface';
+import { initCart } from 'src/app/redux/cart/cart.action';
 
 @Component({
   selector: 'app-cart',
@@ -6,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  [x: string]: any;
 
   prosegui:number;
+  shippingForm: FormGroup;
+  paymentForm: FormGroup;
+  cart: CartItem[];
+  totPrice: number = 0;
   constructor() { }
 
   ngOnInit(): void {
@@ -21,5 +29,25 @@ export class CartComponent implements OnInit {
   back(){
     this.prosegui-=1;
     console.log(this.prosegui)
+  }
+  remove(id:number, price: number){
+    var copy = Array.from(this.cart);
+    const index = copy.findIndex(x=> x.id=== id);
+    copy.splice(index,1);
+    this.cart = copy;
+    this.totPrice-=price;
+    this.updateCart(this.cart);
+  }
+  updateCart(cart: CartItem[] ){
+    this.store.dispatch(initCart({cart}));
+  }
+
+  deleteCurrentCart(){
+    let cart: CartItem[] = [];
+    this.store.dispatch(initCart({cart}));
+  }
+
+  submitForm(){
+    console.log("Shipping for submitted" + this.shippingForm.value)
   }
 }
